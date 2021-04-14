ebcbe4da466c5841205597b1139e69067e156817
s120cc <52039734+s120cc@users.noreply.github.com>
2021-04-13 20:47:40 -0400

    Cleaned up DBHandler.java

--- app/src/main/java/ca/on/conestogac/puppypal/activities/AddAssistantActivity.java
+++ app/src/main/java/ca/on/conestogac/puppypal/activities/AddAssistantActivity.java
@@ -22,6 +22,7 @@ import java.util.List;
 import ca.on.conestogac.puppypal.DBHandler;
 import ca.on.conestogac.puppypal.MainActivity;
 import ca.on.conestogac.puppypal.R;
+import ca.on.conestogac.puppypal.tables.Assistant;
 
 public class AddAssistantActivity extends AppCompatActivity implements Validator.ValidationListener
 {
@@ -63,9 +64,7 @@ public class AddAssistantActivity extends AppCompatActivity implements Validator
         setTheme(R.style.Theme_PuppyPal);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assistant);
-        //added this
         assistantId  = getIntent().getIntExtra("id", -1);
-
         dbHandler = new DBHandler(this);
         nameEditText = findViewById(R.id.textAssistantName);
         phoneNumberEditText = findViewById(R.id.textPhoneNumber);
@@ -73,19 +72,11 @@ public class AddAssistantActivity extends AppCompatActivity implements Validator
         titleEditText = findViewById(R.id.textTitle);
         generalDescriptionEditText = findViewById(R.id.textGeneralDescription);
         Button addAssistantButton = findViewById(R.id.addAssistantToDatabase);
-        //this is unnecessary, text is already set to be this string
-        //addAssistantButton.setText(R.string.add_an_assistant);
         Button deleteAssistantButton = findViewById(R.id.deleteAssistantFromDatabase);
-        //also unnecessary
-        //deleteAssistantButton.setVisibility(View.INVISIBLE);
 
-
-        //added this
         validator = new Validator(this);
         validator.setValidationListener(this);
 
-
-        //changed intent.getIntExtra("id", -1) to assistantId
         if (assistantId != -1)
         {
             addAssistantButton.setText(R.string.update_assistant);
@@ -107,8 +98,6 @@ public class AddAssistantActivity extends AppCompatActivity implements Validator
             });
             System.out.println("Success: Clicked Id is " + assistantId);
 
-
-            //added this
             ArrayList<String> assistant = dbHandler.ReadSingleEntry(Integer.toString(assistantId), "tbl_assistant");
             System.out.println("Received name is: " + assistant.get(1));
             nameEditText.setText(assistant.get(1));
@@ -116,263 +105,39 @@ public class AddAssistantActivity extends AppCompatActivity implements Validator
             addressEditText.setText(assistant.get(3));
             titleEditText.setText(assistant.get(4));
             generalDescriptionEditText.setText(assistant.get(5));
-
-            /**
-             * everything seen below can be done with the block of code above.
-             *
-             * We should never be using a cursor to access the database outside of the DBHandler class
-             */
-            /*
-            Cursor cursor = EditAssistantActivity.cursor;
-            cursor.moveToFirst();
-            if (cursor.getInt(cursor.getColumnIndex("assistant_id")) == intent.getIntExtra("id", -1))
-            {
-                System.out.println("Received name is: " + cursor.getString(cursor.getColumnIndex("name")));
-                nameEditText.setText(cursor.getString(cursor.getColumnIndex("name")));
-                phoneNumberEditText.setText(cursor.getString(cursor.getColumnIndex("phone_number")));
-                addressEditText.setText(cursor.getString(cursor.getColumnIndex("address")));
-                titleEditText.setText(cursor.getString(cursor.getColumnIndex("title")));
-                generalDescriptionEditText.setText(cursor.getString(cursor.getColumnIndex("general_description")));
-            }
-            while (cursor.moveToNext())
-            {
-                if (cursor.getInt(cursor.getColumnIndex("assistant_id")) == intent.getIntExtra("id", -1))
-                {
-                    System.out.println("Received name is: " + cursor.getString(cursor.getColumnIndex("name")));
-                    nameEditText.setText(cursor.getString(cursor.getColumnIndex("name")));
-                    phoneNumberEditText.setText(cursor.getString(cursor.getColumnIndex("phone_number")));
-                    addressEditText.setText(cursor.getString(cursor.getColumnIndex("address")));
-                    titleEditText.setText(cursor.getString(cursor.getColumnIndex("title")));
-                    generalDescriptionEditText.setText(cursor.getString(cursor.getColumnIndex("general_description")));
-                }
-            }
-
-             */
-
-            //nameEditText.setText(EditAssistantActivity.assistantArrayMap.get(intent.getIntExtra("id",-1)));
         }
         addAssistantButton.setOnClickListener(view -> {
             if (assistantId != -1)
             {
                 System.out.println("Update Method Called");
-                Add_the_Assistant(true, assistantId);
+                Add_the_Assistant();
             }
             else
             {
                 System.out.println("Add Method Called");
-                Add_the_Assistant(false, -1);
+                Add_the_Assistant();
             }
         });
     }
 
-    public void Add_the_Assistant(boolean isUpdate, int updateID)
+    public void Add_the_Assistant()
     {
-        /*
-        if (!nameValidation(nameEditText.getText().toString()))
-        {
-            return;
-        }
-        if (!phoneNumberValidation(phoneNumberEditText.getText().toString()))
-        {
-            return;
-        }
-        if (!addressValidation(addressEditText.getText().toString()))
-        {
-            return;
-        }
-        if (!titleValidation(titleEditText.getText().toString()))
-        {
-            return;
-        }
-        if (!generalDescriptionValidation(generalDescriptionEditText.getText().toString()))
-        {
-            return;
-        }
-         */
-
-        //easier validation
         validator.validate();
 
-
-        /*no need to check isUpdate, just pass it
-        if (!isUpdate)
-        {
-         */
-            dbHandler.addAssistantToDB(nameEditText.getText().toString(), phoneNumberEditText.getText().toString(), addressEditText.getText().toString(), titleEditText.getText().toString(), generalDescriptionEditText.getText().toString(), isUpdate, updateID);
-        /*
-        }
-        else
-        {
-            dbHandler.addAssistantToDB(nameEditText.getText().toString(), phoneNumberEditText.getText().toString(), addressEditText.getText().toString(), titleEditText.getText().toString(), generalDescriptionEditText.getText().toString(), isUpdate, updateID);
-        }
-         */
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         Toast.makeText(this, "Assistant has added to the Database", Toast.LENGTH_SHORT).show();
     }
 
-    public void deleteAssistant(int deleteID)
+    public void deleteAssistant(Integer deleteID)
     {
-        dbHandler.deleteAssistant(deleteID);
+        dbHandler.DeleteFromTable(Assistant.TABLE_NAME,Assistant.PRIMARY_KEY,Long.parseLong(deleteID.toString()));
+
         Toast.makeText(this, "Assistant has deleted from the database", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-
-    /*
-    This class is never used, is it necessary?
-     */
-    private AlertDialog AskOption()
-    {
-        return new AlertDialog.Builder(this)
-                // set message, title, and icon
-                .setTitle("Delete")
-                .setMessage("Do you want to Delete")
-                .setIcon(R.drawable.ic_warning_outline)
-
-                .setPositiveButton("Delete", (dialog, whichButton) -> {
-                    //your deleting code
-                    dialog.dismiss();
-                })
-                .setNegativeButton("cancel", (dialog, which) -> dialog.dismiss())
-                .create();
-    }
-
-    /*
-    Replaced with an easier validator
-
-
-    public void alertDialogBuilder(String title, String message)
-    {
-        new AlertDialog.Builder(this)
-                .setIcon(R.drawable.ic_warning_outline)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("Ok", null)
-                .show();
-    }
-
-    public boolean nameValidation(String name)
-    {
-        boolean nameBoolean = false;
-        if (name.isEmpty())
-        {
-            alertDialogBuilder("Name field is empty !", "Name can't be empty");
-            return nameBoolean;
-        }
-
-        if (!name.matches("^([a-zA-Z]+[ ])*[a-zA-Z]+$"))
-        {
-            alertDialogBuilder("Wrong Pattern", "Name should only contains alphabetic letters");
-            return nameBoolean;
-        }
-
-        if (name.length() > 20)
-        {
-            alertDialogBuilder("Text limit exceeds !", "Name should be no more than 20 characters long");
-            return nameBoolean;
-        }
-
-        nameBoolean = true;
-        return nameBoolean;
-    }
-
-    public boolean phoneNumberValidation(String number)
-    {
-        boolean numberBoolean = false;
-        if (number.isEmpty())
-        {
-            alertDialogBuilder("Phone number field is empty !", "Phone Number can't be empty");
-            return numberBoolean;
-        }
-
-        if (number.length() < 10)
-        {
-            alertDialogBuilder("Number is too short !", "Phone Number should be minimum of 10 characters long");
-            return numberBoolean;
-        }
-
-        if (number.length() > 10)
-        {
-            alertDialogBuilder("Number is too long !", "Phone Number should not be more than 10 characters long");
-            return numberBoolean;
-        }
-
-        numberBoolean = true;
-        return numberBoolean;
-    }
-
-    public boolean addressValidation(String address)
-    {
-        boolean addressBoolean = false;
-        if (address.isEmpty())
-        {
-            alertDialogBuilder("Address field is empty !", "Adress can't be empty");
-            return addressBoolean;
-        }
-
-        if (address.length() > 50)
-        {
-            alertDialogBuilder("Text limit exceeds !", "Address should be no more than 50 characters long");
-            return addressBoolean;
-        }
-
-        addressBoolean = true;
-        return addressBoolean;
-    }
-
-    public boolean titleValidation(String title)
-    {
-        boolean titleBoolean = false;
-        if (title.isEmpty())
-        {
-            alertDialogBuilder("Title field is empty !", "Title can't be empty");
-            return titleBoolean;
-        }
-
-        if (!title.matches("^([a-zA-Z]+[ ])*[a-zA-Z]+$"))
-        {
-            alertDialogBuilder("Wrong Pattern", "Title should only contains alphabetic letters");
-            return titleBoolean;
-        }
-
-        if (title.length() > 10)
-        {
-            alertDialogBuilder("Text limit exceeds !", "Title should be no more than 10 characters long");
-            return titleBoolean;
-        }
-
-        titleBoolean = true;
-        return titleBoolean;
-    }
-
-    public boolean generalDescriptionValidation(String generalDescription)
-    {
-        boolean generalDescriptionBoolean = false;
-        if (generalDescription.isEmpty())
-        {
-            alertDialogBuilder("General Description field is empty !", "General Description can't be empty");
-            return generalDescriptionBoolean;
-        }
-
-        if (!generalDescription.matches("^([a-zA-Z0-9,.]+[ ])*[a-zA-Z0-9,.]+$"))
-        {
-            alertDialogBuilder("Wrong Pattern", "General Description should only contains alphabetic letters");
-            return generalDescriptionBoolean;
-        }
-
-        if (generalDescription.length() > 50)
-        {
-            alertDialogBuilder("Text limit exceeds !", "General Description should be no more than 50 characters long");
-            return generalDescriptionBoolean;
-        }
-
-        generalDescriptionBoolean = true;
-        return generalDescriptionBoolean;
-    }
-
-     */
 
     @Override
     public void onValidationSucceeded()
--- app/src/main/java/ca/on/conestogac/puppypal/activities/AddPetActivity.java
+++ app/src/main/java/ca/on/conestogac/puppypal/activities/AddPetActivity.java
@@ -27,8 +27,6 @@ import ca.on.conestogac.puppypal.tables.WeightRecord;
 
 public class AddPetActivity extends AppCompatActivity implements Validator.ValidationListener
 {
-    DBHandler database;
-
     @NotEmpty
     @Length(min = 3, max = 20)
     @Pattern(regex = "^[a-zA-Z]*$")
@@ -47,9 +45,8 @@ public class AddPetActivity extends AppCompatActivity implements Validator.Valid
     @Max(value = 900)
     private EditText textWeight;
 
+    DBHandler database;
     private Validator validator;
-
-    private Pet pet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -58,7 +55,6 @@ public class AddPetActivity extends AppCompatActivity implements Validator.Valid
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_pet);
         database = new DBHandler(this);
-        pet = new Pet();
 
         textName = findViewById(R.id.textName);
         textAge = findViewById(R.id.textAge);
@@ -84,7 +80,7 @@ public class AddPetActivity extends AppCompatActivity implements Validator.Valid
         pet.setAge(Integer.parseInt(textAge.getText().toString()));
         pet.setBreed(textBreed.getText().toString());
 
-        Float weight = Float.parseFloat(textWeight.getText().toString());
+        float weight = Float.parseFloat(textWeight.getText().toString());
         int gender = ((RadioGroup) findViewById(R.id.genderGroup)).getCheckedRadioButtonId();
         boolean spayedNeutered = ((Switch) findViewById(R.id.switchSpayedNeutered)).isChecked();
 
@@ -114,7 +110,6 @@ public class AddPetActivity extends AppCompatActivity implements Validator.Valid
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.ZONE_OFFSET, 0);
         calendar.set(Calendar.DST_OFFSET, 0);
-
         ArrayList<String> petId = database.ReadSingleColumn(Pet.PRIMARY_KEY, Pet.TABLE_NAME, Pet.PRIMARY_KEY + " DESC", "1");
         WeightRecord weightRecord = new WeightRecord(weight, calendar.getTime(), Long.parseLong(petId.get(0)));
         database.AddToTable(WeightRecord.TABLE_NAME, weightRecord.toArray());
