bddd110d416310072430f6953142f1e690febdfd
Kamaljit K <kkambo4651@conestogac.on.ca>
2021-04-25 21:59:05 -0400

    Pushing PuppyPal project to GitHub.

---
+++ app/src/test/java/ca/on/conestogac/puppypal/activities/AddFitnessGoalActivityTest.java
@@ -0,0 +1,132 @@
+package ca.on.conestogac.puppypal.activities;
+
+import org.junit.Test;
+
+import ca.on.conestogac.puppypal.tables.Pet;
+
+import static org.junit.Assert.*;
+
+public class AddFitnessGoalActivityTest {
+    /*
+    @Test
+    public void weightValidation_inputValid() {
+        String input = "100";
+        boolean output;
+        boolean expected = true;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.WeightValidation(input);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void weightValidation_inputEmpty() {
+        String input = "";
+        boolean output;
+        boolean expected = false;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.WeightValidation(input);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void weightValidation_inputGreaterThan999() {
+        String input = "1000";
+        boolean output;
+        boolean expected = false;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.WeightValidation(input);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void energyValidation_inputValid() {
+        String input = "5";
+        boolean output;
+        boolean expected = true;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.EnergyValidation(input);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void energyValidation_inputEmpty() {
+        String input = "";
+        boolean output;
+        boolean expected = false;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.EnergyValidation(input);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void exerciseValidation_exerciseType_inputValid() {
+        String input_1 = "Walk";
+        String input_2 = "30";
+        boolean output;
+        boolean expected = true;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.ExerciseValidation(input_1, input_2);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void exerciseValidation_exerciseType_inputEmpty() {
+        String input_1 = "";
+        String input_2 = "30";
+        boolean output;
+        boolean expected = false;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.ExerciseValidation(input_1, input_2);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void exerciseValidation_exerciseDuration_inputValid() {
+        String input_1 = "Walk";
+        String input_2 = "30";
+        boolean output;
+        boolean expected = true;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.ExerciseValidation(input_1, input_2);
+
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void exerciseValidation_exerciseDuration_inputEmpty() {
+        String input_1 = "Walk";
+        String input_2 = "";
+        boolean output;
+        boolean expected = false;
+
+        AddFitnessGoalActivity addFitnessGoalActivity = new AddFitnessGoalActivity();
+
+        output = addFitnessGoalActivity.ExerciseValidation(input_1, input_2);
+
+        assertEquals(expected, output);
+    }
+    */
+}
